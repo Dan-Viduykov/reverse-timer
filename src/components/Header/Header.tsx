c1f0781c1db15475d9e95d6602312aeca77971a4
Danya_Vid <danilviduykov@gmail.com>
2022-05-29 10:20:37 +0500

    add chooze time

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,23 +1,40 @@
-import React, { FC } from "react";
+import React, { FC, useRef, useState } from "react";
 import { DatePicker } from "antd";
 import "antd/dist/antd.css";
 import './Header.scss'
 
 interface HeaderProps {
-    setDate: (value: any) => void;
+    setDate: (value: string) => void;
 }
 
 const Header: FC<HeaderProps> = ({ setDate }) => {
+    const [ valueDate, setValueDate ] = useState<string>(`${new Date().getFullYear()}-${new Date().getMonth()+1}-${new Date().getDate()}`)
+    const valueTime = useRef<HTMLInputElement | null>(null)
 
-    const handleChange = (value: any, dateString: string) => {
-        setDate(dateString)
-    };
+    const onChange = (value: any, stringValue: string) => setValueDate(stringValue)
+
+    const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+
+        const date = `${valueDate} ${valueTime.current?.value}`;
+        setDate(date);
+    }
 
     return (
-        <div className="header">
+        <header className="header">
             <h1 className="header__title">until the date left</h1>
-            <DatePicker className="header__date" onChange={handleChange}/>
-        </div>
+            <form 
+                className="header__date-time"
+                onSubmit={handleSubmit} >
+                <DatePicker className="header__date" onChange={onChange} />
+                <input
+                    className="header__time"
+                    type="time"
+                    ref={valueTime}
+                    defaultValue={'00:00'} />
+                <button className="header__button-submit" type="submit"></button>
+            </form>
+        </header>
     )
 }
 
